344c43fae7fca2d0593c1275209c3753f3172567
Romee Groothof <rgrootho@f0r5s7.codam.nl>
2021-03-30 17:56:55 +0200

    sausje

--- libft.h
+++ libft.h
@@ -10,9 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef libft_h
-#define libft_h
-#include <stddef.h>
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
@@ -42,9 +49,4 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-typedef struct 		s_list
-{
-	void 			*content;
-	struct s_list 	*next;
-}					t_list;
 #endif
