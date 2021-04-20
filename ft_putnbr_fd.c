5abe540b69ca8f48a61b04d006a2c4cd404bf38d
Romee Groothof <rgrootho@f0r2s9.codam.nl>
2021-04-20 16:52:14 +0200

    fixed putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -5,73 +5,59 @@
 /*                                                     +:+                    */
 /*   By: rgrootho <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2021/01/15 18:33:08 by rgrootho      #+#    #+#                 */
-/*   Updated: 2021/03/26 13:44:13 by rgrootho      ########   odam.nl         */
+/*   Created: 2021/04/20 15:38:47 by rgrootho      #+#    #+#                 */
+/*   Updated: 2021/04/20 16:51:24 by rgrootho      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <unistd.h>
-#include <stdio.h>
 
-int	intlen(int n)
+static int	nlen(int n)
 {
 	int	i;
+	int	pow;
 
 	i = 0;
-	while (n != 0)
+	pow = 1;
+	while (n > 0)
 	{
 		n = n / 10;
 		i++;
 	}
-	return (i);
-}
-
-void	n_is_neg(int n, int numlen, int fd)
-{
-	char	array[numlen + 2];
-	char	chrnbr;
-
-	array[numlen + 1] = '\0';
-	array[0] = '-';
-	n = n * -1;
-	while (n > 0)
+	while (i > 0)
 	{
-		chrnbr = n % 10 + '0';
-		array[numlen] = chrnbr;
-		numlen--;
-		n = n / 10;
+		pow = pow * 10;
+		i--;
 	}
-	ft_putstr_fd(array, fd);
-}
-
-void	n_is_pos(int n, int numlen, int fd)
-{
-	char	array[numlen + 1];
-	char	chrnbr;
-
-	array[numlen] = '\0';
-	while (n > 0)
-	{
-		chrnbr = n % 10 + '0';
-		array[numlen - 1] = chrnbr;
-		numlen--;
-		n = n / 10;
-	}
-	ft_putstr_fd(array, fd);
+	return (pow / 10);
 }
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	int	numlen;
+	char	num2char;
+	int		pow;
 
-	numlen = intlen(n);
 	if (n == 0)
-		ft_putchar_fd('0', fd);
+	{
+		write(fd, "0", 1);
+		return ;
+	}
 	if (n == -2147483648)
-		ft_putstr_fd("-2147483648", fd);
-	else if (n < 0)
-		n_is_neg(n, numlen, fd);
-	else
-		n_is_pos(n, numlen, fd);
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n = n * -1;
+	}
+	pow = nlen(n);
+	num2char = n / (pow) + '0';
+	write(fd, &num2char, 1);
+	n = n % pow;
+	if (n > 0)
+		ft_putnbr_fd(n, fd);
+	return ;
 }
